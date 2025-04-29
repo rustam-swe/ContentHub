6f84c2eb32d20d162aea3e7bff04951a3d08a2d6
AkmalovAbduvoris <abduvorisakmalov5005@gmail.com>
2025-04-29 20:42:33 +0500

    MINOR: installed breeze

--- routes/web.php
+++ routes/web.php
@@ -1,43 +1,37 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
+use Inertia\Inertia;
 use App\Http\Controllers\ContentController;
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
-    return view('home');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-Route::get('/authors', [AuthorController::class, 'index']);
-Route::get('/authors/create', [AuthorController::class, 'create']);
-Route::post('/authors', [AuthorController::class, 'store']);
-Route::get('/authors/{author}', [AuthorController::class, 'show']);
-Route::get('/authors/{author}/edit', [AuthorController::class, 'edit']);
-Route::put('/authors/{author}', [AuthorController::class, 'update']);
-Route::delete('/authors/{author}', [AuthorController::class, 'destroy']);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/genres', [GenreController::class, 'index']);
-Route::get('/genres/create', [GenreController::class, 'create']);
-Route::post('/genres', [GenreController::class, 'store']);
-Route::get('/genres/{genre}', [GenreController::class, 'show']);
-Route::get('/genres/{genre}/edit', [GenreController::class, 'edit']);
-Route::put('/genres/{genre}', [GenreController::class, 'update']);
-Route::delete('/genres/{genre}', [GenreController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/create', [CategoryController::class, 'create']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{category}', [CategoryController::class, 'show']);
-Route::get('/categories/{category}/edit', [CategoryController::class, 'edit']);
-Route::put('/categories/{category}', [CategoryController::class, 'update']);
-Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+require __DIR__.'/auth.php';
 
-Route::get('/contents', [ContentController::class, 'index']);
-Route::get('/contents/create', [ContentController::class, 'create']);
-Route::post('/contents', [ContentController::class, 'store']);
-Route::get('/contents/{content}', [ContentController::class, 'show']);
-Route::get('/contents/{content}/edit', [ContentController::class, 'edit']);
-Route::put('/contents/{content}', [ContentController::class, 'update']);
-Route::delete('/contents/{content}', [ContentController::class, 'destroy']);
+Route::get('/', [ContentController::class, 'index']);
+Route::resource('authors', AuthorController::class);
+Route::resource('genres', GenreController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('contents', ContentController::class);
